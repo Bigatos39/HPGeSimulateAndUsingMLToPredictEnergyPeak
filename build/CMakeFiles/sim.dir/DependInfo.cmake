9bbb16a6a819bb9f28bce1284c29d0ced94548fd
Bigatos39 <162310278+Bigatos39@users.noreply.github.com>
2024-11-03 22:22:08 +0700

    update simulation

---
+++ build/CMakeFiles/sim.dir/DependInfo.cmake
@@ -0,0 +1,30 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/nguyenhoangkhavi/Desktop/HPGeSimulation/testCode/MySimulation.cc" "CMakeFiles/sim.dir/MySimulation.cc.o" "gcc" "CMakeFiles/sim.dir/MySimulation.cc.o.d"
+  "/Users/nguyenhoangkhavi/Desktop/HPGeSimulation/testCode/src/MyActionInitialization.cc" "CMakeFiles/sim.dir/src/MyActionInitialization.cc.o" "gcc" "CMakeFiles/sim.dir/src/MyActionInitialization.cc.o.d"
+  "/Users/nguyenhoangkhavi/Desktop/HPGeSimulation/testCode/src/MyDetectorConstruction.cc" "CMakeFiles/sim.dir/src/MyDetectorConstruction.cc.o" "gcc" "CMakeFiles/sim.dir/src/MyDetectorConstruction.cc.o.d"
+  "/Users/nguyenhoangkhavi/Desktop/HPGeSimulation/testCode/src/MyEventAction.cc" "CMakeFiles/sim.dir/src/MyEventAction.cc.o" "gcc" "CMakeFiles/sim.dir/src/MyEventAction.cc.o.d"
+  "/Users/nguyenhoangkhavi/Desktop/HPGeSimulation/testCode/src/MyPhysicList.cc" "CMakeFiles/sim.dir/src/MyPhysicList.cc.o" "gcc" "CMakeFiles/sim.dir/src/MyPhysicList.cc.o.d"
+  "/Users/nguyenhoangkhavi/Desktop/HPGeSimulation/testCode/src/MyPrimaryGeneratorAction.cc" "CMakeFiles/sim.dir/src/MyPrimaryGeneratorAction.cc.o" "gcc" "CMakeFiles/sim.dir/src/MyPrimaryGeneratorAction.cc.o.d"
+  "/Users/nguyenhoangkhavi/Desktop/HPGeSimulation/testCode/src/MyRunAction.cc" "CMakeFiles/sim.dir/src/MyRunAction.cc.o" "gcc" "CMakeFiles/sim.dir/src/MyRunAction.cc.o.d"
+  "/Users/nguyenhoangkhavi/Desktop/HPGeSimulation/testCode/src/MySteppingAction.cc" "CMakeFiles/sim.dir/src/MySteppingAction.cc.o" "gcc" "CMakeFiles/sim.dir/src/MySteppingAction.cc.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/sim.dir/cmake_clean.cmake
@@ -0,0 +1,25 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sim.dir/MySimulation.cc.o"
+  "CMakeFiles/sim.dir/MySimulation.cc.o.d"
+  "CMakeFiles/sim.dir/src/MyActionInitialization.cc.o"
+  "CMakeFiles/sim.dir/src/MyActionInitialization.cc.o.d"
+  "CMakeFiles/sim.dir/src/MyDetectorConstruction.cc.o"
+  "CMakeFiles/sim.dir/src/MyDetectorConstruction.cc.o.d"
+  "CMakeFiles/sim.dir/src/MyEventAction.cc.o"
+  "CMakeFiles/sim.dir/src/MyEventAction.cc.o.d"
+  "CMakeFiles/sim.dir/src/MyPhysicList.cc.o"
+  "CMakeFiles/sim.dir/src/MyPhysicList.cc.o.d"
+  "CMakeFiles/sim.dir/src/MyPrimaryGeneratorAction.cc.o"
+  "CMakeFiles/sim.dir/src/MyPrimaryGeneratorAction.cc.o.d"
+  "CMakeFiles/sim.dir/src/MyRunAction.cc.o"
+  "CMakeFiles/sim.dir/src/MyRunAction.cc.o.d"
+  "CMakeFiles/sim.dir/src/MySteppingAction.cc.o"
+  "CMakeFiles/sim.dir/src/MySteppingAction.cc.o.d"
+  "sim"
+  "sim.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sim.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
